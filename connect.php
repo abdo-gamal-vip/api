ffd328946e8cf36ddb4172ef2d0042d27f8746fd
abdo-gamal-vip <abdo.gamal.vip@gmail.com>
2022-08-01 19:59:12 +0200

    modified:   connect.php

--- connect.php
+++ connect.php
@@ -6,4 +6,11 @@ $oprion=array(
     PDO:: MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8",
 );
 $conct=new PDO($dsn,$user,$pass,$oprion);
+try {
+$conct = new PDO($dsn, $user, $pass, $oprion);
+$conct -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} 
+catch(PDOException $e){
+    echo $e->getMessage();
+}
 ?>
