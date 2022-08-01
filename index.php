6afade450e24dcc61ac2576493f77f55dba047e2
abdo-gamal-vip <abdo.gamal.vip@gmail.com>
2022-08-01 19:36:54 +0200

    modified:   index.php

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
-$listname = array("body","zizo","fofo",4);
-$info = array(
-"name"=>"abdo","age"=>23
-
-);
-
-print_r($listname);
-for($i=0;$i < count($listname);$i++){
-    echo $listname[$i]."<br>";
-}
-?>
-<?php
-echo "<br> ad <br> sa";
+$name = $_GET['name'];
+$last = $_GET['last'];
+$age = $_GET['age'];
+ print_r($_GET);
 ?>
