32eefcc2749c0395a494631e8ffba19145b765c9
abdo-gamal-vip <abdo.gamal.vip@gmail.com>
2022-08-04 14:34:37 +0200

    Changes to be committed: 	modified:   connect.php
    Changes not staged for commit:
    	modified:   index.php

--- connect.php
+++ connect.php
@@ -2,16 +2,16 @@
 $dsn = "mysql:host=localhost;dbname=e_commerce_app";
 $user="root";
 $pass="";
-$oprion=array(
+$option=array(
     PDO:: MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8",
 );
-$conct=new PDO($dsn,$user,$pass,$oprion);
 try {
-$conct = new PDO($dsn, $user, $pass, $oprion);
+$conct = new PDO($dsn, $user, $pass, $option);
 $conct -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } 
 catch(PDOException $e){
     echo $e->getMessage();
-    echo "hello <b/>";
+    
 }
 ?>
+// Path: connect.php
