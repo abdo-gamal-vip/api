d98506f1592794fda41bcf1a1e2378b2b2fb4dd5
abdo-gamal-vip <abdo.gamal.vip@gmail.com>
2022-08-13 21:47:33 +0200

    modified:   test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-$fileName ="images.jpg";
+$fileName ="images.txt";
 $texttoarray =explode(".", $fileName);
 $ext =end($texttoarray);
 $allowtext=array("jpg","png");
@@ -7,4 +7,10 @@ $allowtext=array("jpg","png");
 echo "<pre>";
 print_r($ext);
 echo "</pre>";
+
+if(in_array($ext , $allowtext)){
+    echo "yes";
+}else {
+    print "no";
+}
 ?>
