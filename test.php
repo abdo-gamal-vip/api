b6a53f295d5efc186c53fce8f44c88b3bf876cf6
abdo-gamal-vip <abdo.gamal.vip@gmail.com>
2022-08-13 14:28:04 +0200

    modified:   functions.php 	modified:   test.php 	new file:   upload/12.png

--- test.php
+++ test.php
@@ -1,10 +1,4 @@
 <?php
-$fileName ="images.jpg";
-$texttoarray =explode(".", $fileName);
-$ext =end($texttoarray);
-$allowtext=array("jpg","png");
-
-echo "<pre>";
-print_r($ext);
-echo "</pre>";
-?>
+include "connect.php";
+imageUpload('file');
+?> 
